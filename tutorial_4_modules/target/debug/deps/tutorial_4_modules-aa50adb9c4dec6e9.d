0bfe76a697ee56f0306cad73e5b7b2151042a5b9
rudy.schueder <rudy.schueder@outlook.com>
2022-06-25 15:22:51 -0700

    starting modules

---
+++ tutorial_4_modules/target/debug/deps/tutorial_4_modules-aa50adb9c4dec6e9.d
@@ -0,0 +1,5 @@
+/home/rudys/projects/rust-book/tutorial_4_modules/target/debug/deps/tutorial_4_modules-aa50adb9c4dec6e9: src/main.rs
+
+/home/rudys/projects/rust-book/tutorial_4_modules/target/debug/deps/tutorial_4_modules-aa50adb9c4dec6e9.d: src/main.rs
+
+src/main.rs:
---
+++ tutorial_4_modules/target/debug/tutorial_4_modules.d
@@ -0,0 +1,1 @@
+/home/rudys/projects/rust-book/tutorial_4_modules/target/debug/tutorial_4_modules: /home/rudys/projects/rust-book/tutorial_4_modules/src/main.rs
