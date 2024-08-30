0614a17e35f816626af145762920957c86c3b6c3
RSchueder <rudy.schueder@outlook.com>
2024-08-29 19:54:31 -0700

    remove bin and lock files

--- tutorial_3_enums/target/rls/debug/deps/tutorial_3_enums-fe86378230ace629.d
+++ tutorial_3_enums/target/rls/debug/deps/tutorial_3_enums-fe86378230ace629.d
@@ -1,5 +1,5 @@
-/Users/rudy/projects/rust/tutorial_3_enums/target/rls/debug/deps/tutorial_3_enums-fe86378230ace629.rmeta: src/main.rs
+/Users/rudy/projects/rust-book/tutorial_3_enums/target/rls/debug/deps/tutorial_3_enums-fe86378230ace629.rmeta: src/main.rs
 
-/Users/rudy/projects/rust/tutorial_3_enums/target/rls/debug/deps/tutorial_3_enums-fe86378230ace629.d: src/main.rs
+/Users/rudy/projects/rust-book/tutorial_3_enums/target/rls/debug/deps/tutorial_3_enums-fe86378230ace629.d: src/main.rs
 
 src/main.rs:
