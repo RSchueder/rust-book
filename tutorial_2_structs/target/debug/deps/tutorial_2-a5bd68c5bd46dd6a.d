48faa80d2a6c13d4798a6527067bc8fd4b1174cb
rschueder <rudy.schueder@outlook.com>
2024-08-31 12:42:55 -0700

    remove all targets

--- tutorial_2_structs/target/debug/deps/tutorial_2-a5bd68c5bd46dd6a.d
+++
@@ -1,5 +0,0 @@
-/home/rudys/projects/rust-book/tutorial_2_structs/target/debug/deps/tutorial_2-a5bd68c5bd46dd6a: src/main.rs
-
-/home/rudys/projects/rust-book/tutorial_2_structs/target/debug/deps/tutorial_2-a5bd68c5bd46dd6a.d: src/main.rs
-
-src/main.rs:
--- tutorial_2_structs/target/debug/tutorial_2.d
+++
@@ -1,1 +0,0 @@
-/home/rudys/projects/rust-book/tutorial_2_structs/target/debug/tutorial_2: /home/rudys/projects/rust-book/tutorial_2_structs/src/main.rs
